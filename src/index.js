1b1444d1b835d060939abe0624e975589a71f5fd
Ankush523 <ankush.ad050203@gmail.com>
2023-02-18 10:37:19 +0530

    goerli, polygon mumbai connections added

--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ import {
   RainbowKitProvider,
   lightTheme
 } from '@rainbow-me/rainbowkit';
-import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
+import { configureChains, createClient, goerli, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum, polygonMumbai } from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, provider } = configureChains(
-  [mainnet, polygon, optimism, arbitrum],
+  [mainnet, goerli, polygon, polygonMumbai],
   [
     alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
     publicProvider()
