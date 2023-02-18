41823f4d65552d25fb2c81857ea1bc05acf1e55e
Ankush523 <ankush.ad050203@gmail.com>
2023-02-18 10:17:54 +0530

    stream tracking added

--- src/pages/Donor.js
+++ src/pages/Donor.js
@@ -18,6 +18,11 @@ const Donor = () => {
           <div className='flex flex-col w-[20%] h-[100vh] border-r border-r-grey1 pt-[80px] pr-[80px]'>
               <button onClick={() => setSelection(1)} className='bg-white1 h-[50px] text-xl text-blue1 font-poppins shadow-2xl rounded-lg mb-[30px] hover:bg-blue1 hover:text-white1'>Intro</button>
               <button onClick={() => setSelection(2)} className='bg-white1 h-[50px] text-xl text-blue1 font-poppins shadow-2xl rounded-lg mb-[30px] hover:bg-blue1 hover:text-white1'>Project Panel</button>
+              <button className='bg-white1 h-[50px] text-xl text-blue1 font-poppins shadow-2xl rounded-lg mb-[30px] hover:bg-blue1 hover:text-white1'>
+                <a href='https://app.superfluid.finance/'>
+                  Track Streams
+                </a>
+              </button>
           </div>
 
           <div className='h-[100vh] w-[80%] px-4 mt-[100px]'>
